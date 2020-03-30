b69170c37915f173e15630011fcbd2072c435f96
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-03-30 16:51:00 -0300

    Update blocksite.user.js

--- blocksite.user.js
+++ blocksite.user.js
@@ -1,1 +1,22 @@
+// ==UserScript==
+// @name         BlockSite
+// @version      0.1
+// @description  Cierra las pestañas no deseadas automaticamente
+// @author       @JaviertINC
+// @match        *://*/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
 
+(function() {
+    'use strict';
+
+    var blocklist = "https://javiertinc.github.io/BlockSite/blocklist.json";
+
+    var this_href = window.location.href;
+    var this_host = window.location.hostname;
+    var this_path = window.location.pathname;
+    var this_prot = window.location.protocol;
+
+
+})();
